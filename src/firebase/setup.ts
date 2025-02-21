548e571c05108bea12cdba70504956c6d138349c
Atte Silen <atzusilen@gmail.com>
2025-02-21 11:45:48 +0200

    Update firebase setup

--- src/firebase/setup.ts
+++ src/firebase/setup.ts
@@ -1,4 +1,6 @@
-export async function setup(settings: any) {
+import { AppSettings } from './types.ts';
+
+export async function setup(settings: AppSettings) {
   globalThis.alexi = { conf: { settings: settings, apps: {}, databases: {} } };
 
   for (const AppConfig of settings.INSTALLED_APPS) {
@@ -6,5 +8,12 @@ export async function setup(settings: any) {
     globalThis.alexi.conf.apps[app.name] = app;
     globalThis.alexi.conf.apps[app.name].models = await app.getModels?.();
     globalThis.alexi.conf.apps[app.name].functions = await app.getFunctions?.();
+  }
+
+  for (const key in settings.DATABASES) {
+    const databaseConfig = settings.DATABASES[key];
+    const database = new databaseConfig.ENGINE(databaseConfig);
+    await database.init();
+    globalThis.alexi.conf.databases[databaseConfig.NAME] = database;
   }
 }
